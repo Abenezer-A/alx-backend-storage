a7b825f007e049a52f7bc570f8adab2007302fb6
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-04-25 14:37:48 -0700

    insert_school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,7 +6,6 @@ a python function that inserts a new document in a collection
 import pymongo
 
 
-
 def insert_school(mongo_collection, **kwargs):
     """
     inserts a new document in a collection based on kwargs
