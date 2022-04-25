787ac988c38775d9e99fea9c5a6fa3e35b0a75bd
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-04-25 14:36:34 -0700

    update_topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,7 +5,6 @@ updates a mongodb collection
 import pymongo
 
 
-
 def update_topics(mongo_collection, name, topics):
     """updates a mongo ducument"""
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
