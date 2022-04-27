ec5fba5ac6af700cdbcf0a26d684329ba96a4a58
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-04-27 12:43:06 -0700

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -15,9 +15,24 @@ def requests_counter(method: Callable) -> Callable:
     """
     r = redis.Redis()
 
+    @wraps(method)
+    def wrapper(url):
+        """ wrapper fxn that counts actual no of requests made"""
+        r.incr(f"count:{url}")
+        cached_ = r.get(f"cached:{url}")
+        if cached:
+            return cached.decode('utf-8')
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
 @requests_counter
 def get_page(url: str) -> str:
     """ obtains html content for a given site url and returns it.
     """
     resp = requests.get(url)
-    return resp.
+    return resp.text
